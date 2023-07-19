e3bd4bc84eb68529a3109510de97213f2798e7d5
Lucio Noleto <luciolnoleto@gmail.com>
2023-07-19 17:01:42 -0300

    Finalizando o Context

--- config/context.js
+++ config/context.js
@@ -1,3 +1,41 @@
-module.exports = ({req}) => {
+const jwt = require('jwt-simple')
+
+module.exports = async ({req}) => {
+    //Em desenvolvimento
+    await require('./simularUsuarioLogado')(req)
+    
+
+    //Em produção
     const auth = req.headers.authorization
+    const token = auth && auth.substring(7)
+
+    let usuario = null
+    let admin = false 
+
+    if(token){
+        try {
+            let conteudoToken = jwt.decode(token, process.env.APP_AUTH_SECRET)
+            if(new Date(conteudoToken.exp * 1000) > new Date()){
+                usuario = conteudoToken
+            }
+        } catch(e){
+            //token invalido
+        }
+    }
+    if(usuario && usuario.perfis){
+        admin = usuario.perfis.includes('admin')
+    }
+
+    const err = new Error('Acesso Negado!')
+
+    return {
+        usuario,
+        admin,
+        validarUsuario(){
+            if(!usuario) throw err
+        },
+        validarAdmin(){
+            if(!admin) throw err
+        }
+    }
 }   
