09eb26000b01536c100b121bd664205551bd8a73
Lucio Noleto <luciolnoleto@gmail.com>
2023-07-19 16:11:48 -0300

    new query: Login + JWT

--- resolvers/Query/usuario.js
+++ resolvers/Query/usuario.js
@@ -1,6 +1,26 @@
 const db = require('../../config/db')
+const bcrypt = require('bcrypt-nodejs')
+const {getUsuarioLogado} = require('../Comum/usuario')
 
 module.exports = {
+    async login(_, {dados}){
+        const usuario = await db('usuarios')
+            .where({email: dados.email})
+            .first()
+        
+        if(!usuario){
+            throw new Error('Usuário/Senha inválido')
+        }
+
+        const saoIguais = bcrypt.compareSync(dados.senha, usuario.senha)
+
+        if(!saoIguais){
+            throw new Error('Senha inválida!')
+        }
+
+        return getUsuarioLogado(usuario)
+    },
+
     usuarios() {
         return db('usuarios')
     },
