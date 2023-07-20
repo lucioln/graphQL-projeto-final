bdf43507b9e316757d97826327818dd955749c7b
Lucio Noleto <luciolnoleto@gmail.com>
2023-07-20 15:44:28 -0300

    Finalizando o Backend

--- resolvers/Query/usuario.js
+++ resolvers/Query/usuario.js
@@ -21,10 +21,12 @@ module.exports = {
         return getUsuarioLogado(usuario)
     },
 
-    usuarios() {
+    usuarios(parent, args, ctx) {
+        ctx && ctx.validarAdmin()
         return db('usuarios')
     },
-    usuario(_, { filtro }) {
+    usuario(_, { filtro }, ctx) {
+        ctx && ctx.validarUsuarioFiltro(filtro)
         if(!filtro) return null
         const { id, email } = filtro
         if(id) {
